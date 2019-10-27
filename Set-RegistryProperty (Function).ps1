32ef779879da84320c742d52e24a61583cf7e284
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2019-10-27 12:25:02 +0100

    Added Set-OfficePersonalTemplates & Set-RegistryProperty (Function)

---
+++ Set-RegistryProperty (Function).ps1
@@ -0,0 +1,18 @@
+﻿Function Set-RegistryProperty {
+    # Version 2.3
+    Param(
+        [Parameter(Mandatory=$true)][string]$RegistryKey,
+        [Parameter(Mandatory=$true)][string]$PropertyName,
+        [Parameter(Mandatory=$true)][string][ValidateSet("String","ExpandString","Binary","DWord","MultiString","Qword")]$PropertyType,
+        [Parameter(Mandatory=$true)][string]$PropertyValue
+    )
+    #Check for registry key path and create if necassary
+    If(!(Test-Path -Path $RegistryKey)){
+        New-Item -Path $RegistryKey -Force
+    }
+
+    #Remove previous itemproperty with a different property type. Error will be ignored if it does not exist.
+    Remove-ItemProperty -Path $RegistryKey -Name $PropertyName -Force -ErrorAction SilentlyContinue
+    #Create new itemproperty with the right settins
+    New-ItemProperty -Path $RegistryKey -Name $PropertyName -PropertyType $PropertyType -Value $PropertyValue -Force
+}
