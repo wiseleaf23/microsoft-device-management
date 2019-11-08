6f91993ef6eb7331f58bf5224946351e17e1646c
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2019-11-08 16:57:46 +0100

    Create-VM & Create-Junction

---
+++ Create-Junction (Function).ps1
@@ -0,0 +1,19 @@
+﻿Function Create-Junction {
+    #Version 1.0
+    Param(
+        [Parameter(Mandatory=$true)] [switch] $Hidden, #Mark the junction as hidden
+        [Parameter(Mandatory=$true)] $Source,
+        [Parameter(Mandatory=$true)] $Destination
+    )
+
+    #Move
+    Move-Item -Path "$Source" -Destination "$Destination"
+
+    #Create Junction
+    New-Item -ItemType Junction -Path $Source -Value $Destination
+
+    #Hide Junction if Hidden is used
+    If($Hidden){
+        (Get-Item -Path $Source).Attributes += 'Hidden' #The method you use to set a hidden attribute is important, you dont want to overwrite existing attributes
+    }
+}
---
+++ Create-VM.ps1
@@ -0,0 +1,73 @@
+﻿#Create a new VM and perform basic config
+Function Create-VM {
+    Param(
+        [Parameter(Mandatory=$true)] [string] $VmName,
+        [Parameter(Mandatory=$false)] [string] $ISO
+    )
+
+    $MemoryStartupBytes = "1073741824"
+    $SwitchName = "Default Switch"
+    $VmPath = "D:"
+    $VmPath = $VmPath.Replace('\','')
+    $VmGeneration = "2"
+    $NewVhdPath = "$VmPath\$VmName\Virtual Hard Disks\$VmName.vhdx"
+    $NewVhdSizeBytes = "136365211648"
+    $VmProcessorCount = "8"
+
+    #Create VM
+    $Args = @{
+        Name = $VmName
+        MemoryStartupBytes = $MemoryStartupBytes
+        #BootDevice = 
+        SwitchName = $SwitchName
+        NewVHDPath = $NewVhdPath
+        NewVHDSizeBytes = $NewVhdSizeBytes
+        Path = $VmPath
+        #Version
+        #Prerelease
+        #Experimental
+        Generation = $VmGeneration
+        #Force
+        #AsJob
+        #CimSession CimSession
+        #ComputerName String
+        #Credential PSCredential
+        #WhatIf
+        #Confirm
+        #CommonParameters
+    }
+        
+    New-VM @Args
+
+    #Additional config
+        #Enable Secure Boot & TPM
+        Set-VMFirmware $VmName -EnableSecureBoot On
+        Set-VMKeyProtector -VMName $VmName -NewLocalKeyProtector
+        Enable-VMTPM -VMName $VmName
+
+        #CPU's
+        Set-VM -Name $VmName -ProcessorCount $VmProcessorCount
+
+        #Add DVD drive & Set boot device
+        If($ISO){
+            $Args = @{
+                #CimSession CimSession
+                #ComputerName String
+                #Credential PSCredential
+                VMName = $VmName
+                #ControllerNumber Int32
+                #ControllerLocation Int32
+                Path = $ISO
+                #ResourcePoolName String
+                #AllowUnverifiedPaths
+                #Passthru
+                #WhatIf
+                #Confirm
+                #CommonParameters
+            }
+            Add-VMDvdDrive @Args
+            $VmDvdDrive = Get-VMDvdDrive -VMName $VmName
+            Set-VMFirmware -VMName $VmName -FirstBootDevice  $VmDvdDrive
+        }
+
+}
