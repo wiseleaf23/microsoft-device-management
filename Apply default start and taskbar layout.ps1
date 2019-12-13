c56c556f5b3288af01d1146ac34d9dfcc8b9860d
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2019-12-13 13:34:33 +0100

    Added some scripts

---
+++ Apply default start and taskbar layout.ps1
@@ -0,0 +1,14 @@
+﻿#Clean the start menu
+#Run in logged on user context, 64-bit PowerShell host
+
+#Variables
+    $Name = "Data"
+    $MainRegistryPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount'
+    $RegistryPath = (Get-ChildItem -Path $MainRegistryPath | Where-Object {$_.PSChildName -match 'start.tilegrid'}).PSPath + "\Current"
+
+#Remove binary data, and restart explorer to rebuild this.
+    Remove-ItemProperty -Path $RegistryPath -Name $Name -Force
+    Stop-Process -Name "explorer" -Force
+    Start-Process "explorer.exe"
+
+#Start menu should now be set to the default profile
