2066a4bae389e9d6ce798bdeadbf4d632f65b740
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2020-01-09 14:23:14 +0100

    Update Install-PSGalleryModule (Function).ps1

--- Install-PSGalleryModule (Function).ps1
+++ Install-PSGalleryModule (Function).ps1
@@ -2,8 +2,8 @@ function Install-PSGalleryModule {
     param (
         [Parameter(Mandatory=$true)][string]$ModuleName
     )
-    #Version 1.0
-    Install-PackageProvider -Name NuGet -Force -Confirm:$false
+    #Version 1.1
+    Install-PackageProvider -Name NuGet -Scope CurrentUser -Force -Confirm:$false
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     Install-Module $ModuleName -Scope CurrentUser -Force -Confirm:$false
 }
