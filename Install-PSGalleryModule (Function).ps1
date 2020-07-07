e8b10d1da7f7e151081d24c896ea6b52092071e0
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2020-07-07 08:49:31 +0200

    Update Install-PSGalleryModule (Function).ps1

--- Install-PSGalleryModule (Function).ps1
+++ Install-PSGalleryModule (Function).ps1
@@ -1,9 +1,11 @@
 function Install-PSGalleryModule {
     param (
-        [Parameter(Mandatory=$true)][string]$ModuleName
+        [Parameter(Mandatory=$true)][string]$ModuleName,
+        [Parameter(Mandatory=$false)][string][ValidateSet("CurrentUser","AllUsers")][PSDefaultValue(Help = 'CurrentUser')]$Scope = 'CurrentUser'
     )
-    #Version 1.1
-    Install-PackageProvider -Name NuGet -Scope CurrentUser -Force -Confirm:$false
+    #Version 1.4
+    Install-PackageProvider -Name NuGet -Scope $Scope -Force -Confirm:$false #Install or update NuGet package provider
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    Install-Module $ModuleName -Scope CurrentUser -Force -Confirm:$false
+    Install-Module PowerShellGet -Scope $Scope -Force -AllowClobber -Confirm:$false #Install or update PowerShellGet
+    Start-Process -FilePath powershell.exe -ArgumentList "-WindowStyle Hidden -Command & {Install-Module $ModuleName -Scope $Scope -Force -AcceptLicense -Confirm:`$false}" -Wait #Start PowerShell in a new session, to prevent any issues with loaded modules etc
 }
