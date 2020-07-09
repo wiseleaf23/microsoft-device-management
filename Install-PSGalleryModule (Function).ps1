66c4cac8dae7fcfa5d13c09f9fb1dba7433d843f
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2020-07-09 10:34:53 +0200

    Update Install-PSGalleryModule (Function).ps1

--- Install-PSGalleryModule (Function).ps1
+++ Install-PSGalleryModule (Function).ps1
@@ -3,9 +3,9 @@ function Install-PSGalleryModule {
         [Parameter(Mandatory=$true)][string]$ModuleName,
         [Parameter(Mandatory=$false)][string][ValidateSet("CurrentUser","AllUsers")][PSDefaultValue(Help = 'CurrentUser')]$Scope = 'CurrentUser'
     )
-    #Version 1.4
-    Install-PackageProvider -Name NuGet -Scope $Scope -Force -Confirm:$false #Install or update NuGet package provider
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    Install-Module PowerShellGet -Scope $Scope -Force -AllowClobber -Confirm:$false #Install or update PowerShellGet
+    #Version 1.5
+    Install-PackageProvider -Name NuGet -Scope $Scope -Force -Confirm:$false #Install most recent NuGet package provider
+    Install-Module PowerShellGet -Scope $Scope -Force -AllowClobber -Confirm:$false #Install most recent PowerShellGet
+    Start-Process -FilePath powershell.exe -ArgumentList "-WindowStyle Hidden -Command & {Set-PSRepository -Name PSGallery -InstallationPolicy Trusted}" -Wait #Start PowerShell in a new session, to prevent any issues with loaded modules etc
     Start-Process -FilePath powershell.exe -ArgumentList "-WindowStyle Hidden -Command & {Install-Module $ModuleName -Scope $Scope -Force -AcceptLicense -Confirm:`$false}" -Wait #Start PowerShell in a new session, to prevent any issues with loaded modules etc
 }
