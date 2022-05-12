a0d1d51bf966cc6ac9eafbfb365c35261624d314
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2022-05-12 14:10:51 +0200

    Update Remove-RegistryProperty (Function).ps1

--- Remove-RegistryProperty (Function).ps1
+++ Remove-RegistryProperty (Function).ps1
@@ -1,13 +1,24 @@
 Function Remove-RegistryProperty {
-    # Version 1.0
+    #Version 1.1
     Param(
-        [Parameter(Mandatory=$true)][string]$RegistryKey,    
+        [Parameter(Mandatory=$true)][string]$RegistryKey,
         [Parameter(Mandatory=$true)][string]$PropertyName
     )
-    Write-Host "Removing property $PropertyName in $RegistryKey" -ForegroundColor Magenta
-    if(Get-ItemProperty -Path $RegistryKey -Name $PropertyName -ErrorAction SilentlyContinue){
-        Remove-ItemProperty -Path $RegistryKey -Name $PropertyName -Force
-    }else{
-        Write-Host "Registry property does not exist, so do nothing" -ForegroundColor Magenta
+    
+    #If property is not specified, we need to delete an entire key, else, only remove property
+    if(!$PropertyName){
+        #Property is not specified, checking for and removing the entire key
+        if(Get-Item -Path $RegistryKey -ErrorAction SilentlyContinue){
+            Remove-Item -Path $RegistryKey -Recurse -Force -Confirm:$false -Verbose
+        }else{
+            Write-Output "Registry key $RegistryKey does not exist, doing nothing"
+        }        
+    }else {
+        #Property is specified, checking for and removing the property
+        if(Get-ItemProperty -Path $RegistryKey -Name $PropertyName -ErrorAction SilentlyContinue){
+            Remove-ItemProperty -Path $RegistryKey -Name $PropertyName -Force -Confirm:$false -Verbose
+        }else{
+            Write-Output "Registry property $PropertyName in $RegistryKey does not exist, doing nothing"
+        }
     }
 }
